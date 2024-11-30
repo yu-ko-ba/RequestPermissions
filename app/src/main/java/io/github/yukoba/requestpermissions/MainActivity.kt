e34e9704131983066e2d40f360a3aa145397388f
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2024-11-30 19:34:35 +0900

    update: sample code

--- app/src/main/java/io/github/yukoba/requestpermissions/MainActivity.kt
+++ app/src/main/java/io/github/yukoba/requestpermissions/MainActivity.kt
@@ -6,8 +6,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
@@ -20,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import io.github.yukoba.requestpermissions.ui.theme.RequestPermissionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,106 +43,58 @@ class MainActivity : ComponentActivity() {
             var whyNeedPermissionsDialogIsShown by remember { mutableStateOf(false) }
 
             RequestPermissionsTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                    ) {
-                        Button(
-                            onClick = {
-                                requestPermissions.request(requiredPermissions) {
-                                    Toast.makeText(
-                                        this@MainActivity,
-                                        "Permissions granted!",
-                                        Toast.LENGTH_LONG,
-                                    )
-                                        .show()
-                                }
-                            },
-                        ) {
-                            Text(
-                                text = "Request permissions",
-                            )
-                        }
-
-                        Button(
-                            modifier = Modifier.padding(top = 32.dp),
-                            onClick = {
-                                requestPermissions.request(
-                                    requiredPermissions,
-                                    onDenied = { deniedPermissions ->
-                                        Toast.makeText(
-                                            this@MainActivity,
-                                            "Denied permissions: $deniedPermissions",
-                                            Toast.LENGTH_LONG,
-                                        )
-                                            .show()
-                                    },
-                                ) {
-                                    Toast.makeText(
-                                        this@MainActivity,
-                                        "Permissions granted!",
-                                        Toast.LENGTH_LONG,
-                                    )
-                                        .show()
-                                }
-                            },
-                        ) {
-                            Text(text = "Request permissions with onDenied")
-                        }
-
-                        Button(
-                            modifier = Modifier.padding(top = 32.dp),
-                            onClick = {
-                                requestPermissions.request(
-                                    requiredPermissions,
-                                    showWhyNeedPermissionsDialog = {
-                                        whyNeedPermissionsOnConfirm = it
-
-                                        whyNeedPermissionsDialogIsShown = true
-                                    },
-                                ) {
-                                    Toast.makeText(
-                                        this@MainActivity,
-                                        "Permissions requested in dialog has been granted!",
-                                        Toast.LENGTH_LONG,
-                                    )
-                                        .show()
-                                }
-                            },
-                        ) {
-                            Text(text = "Request permissions with why need dialog")
-                        }
-                    }
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     if (whyNeedPermissionsDialogIsShown) {
                         AlertDialog(
                             onDismissRequest = { whyNeedPermissionsDialogIsShown = false },
                             dismissButton = {
                                 Button(
                                     onClick = { whyNeedPermissionsDialogIsShown = false }
-                                ) {
-                                    Text(text = "Cancel")
-                                }
+                                ) { Text(text = "Cancel") }
                             },
                             confirmButton = {
                                 Button(
                                     onClick = {
                                         whyNeedPermissionsDialogIsShown = false
-
                                         whyNeedPermissionsOnConfirm()
                                     },
-                                ) {
-                                    Text(text = "OK")
-                                }
+                                ) { Text(text = "OK") }
                             },
-                            text = { Text(text = "Reason of requesting permissions") },
+                            text = { Text(text = "Need permissions to work.") },
                         )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Button(
+                            onClick = {
+                                requestPermissions.request(
+                                    permissions = requiredPermissions,
+                                    showWhyNeedPermissionsDialog = {
+                                        whyNeedPermissionsOnConfirm = it
+                                        whyNeedPermissionsDialogIsShown = true
+                                    },
+                                    onGranted = {
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Permissions granted!",
+                                            Toast.LENGTH_LONG,
+                                        ).show()
+                                    },
+                                    onDenied = {
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Permissions denied.",
+                                            Toast.LENGTH_LONG,
+                                        ).show()
+                                    },
+                                )
+                            },
+                        ) { Text(text = "Request permissions") }
                     }
                 }
             }
